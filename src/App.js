66e73ae7031d1c5ed47e0ff7214cce764bab0c85
João Victor Simonassi Farias <joao.farias@radixeng.com.br>
2020-10-14 23:31:06 -0300

    Adicionando novo box

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
           <div className='boxGrid'><Box /></div>
           <div className='boxGrid'><Box /></div>
           <div className='boxGrid'><Box /></div>
+          <div className='boxGrid'><Box /></div>
         </div>
       </div>
     </div>
